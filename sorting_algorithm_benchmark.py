32954d973b394064b36913976677950470b2bc57
olgarozhdestvina <olgarozhdestvina@gmail.com>
2021-04-20 18:27:22 +0800

    Removed the decorator and moved time difference calculations into the benchmark function. Added a quicksort function. Removed a save_as_table function - instead saving the file in excel format. Added tabulate print out into the console. Moved legend to the outside of the plot.

--- sorting_algorithm_benchmark.py
+++ sorting_algorithm_benchmark.py
@@ -6,40 +6,20 @@ Completed by Olga Rozhdestvina
 
 Application to benchmark five different sorting algorithms.
 Implementations are taken from:
-Insertion sort: https://realpython.com/sorting-algorithms-python/#implementing-insertion-sort-in-python
+Santiago Valdarrama, Real Python, Sorting Algorithms in Python,[ONLINE] Available at: https://realpython.com/sorting-algorithms-python/. [Accessed 20 April 2021].
+
 """
 
 from time import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from pandas.plotting import table
 
 
-
-def performance(func):
-    """ Decorator that measures performace of a sorting algorithm """
-    def wrap_func(*args, **kwargs):
-
-        # Register the time before and after running a sorting algorithm
-        # And calculate the difference.
-        time1 = time()
-        res = func(*args, **kwargs)
-        time_diff = time() - time1
-        return res, time_diff
-
-    return wrap_func
-
-
-def shuffled_array(array):
-    """ Shuffle an array using a numpy random number generator """
-    rng = np.random.default_rng()
-    rng.shuffle(array)
-    return array
-
+""" ALGORITHMS """
 
 # 1. Insertion sort
-@performance
+
 def insertion_sort(array):
 
     # Loop through the array starting from its second element
@@ -67,11 +47,51 @@ def insertion_sort(array):
     return array
 
 
+# 2. Quicksort.
+
+def quicksort(array):
+    # Base case for the recursion
+    if len(array) < 2:
+        return array
+
+    # Randomly select a pivot
+    pivot = array[np.random.randint(0, len(array) - 1)]
+
+    # Loop through the array and compare each element to the pivot. 
+    # If they are smaller --> add to the low list.
+    # If bigger --> to the high list.
+    # If same --> the same list.
+    low, same, high = [], [], []
+
+    for item in array:
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        else:
+            high.append(item)
+
+    # Combine the lists into one in the low-same-high order.
+    return quicksort(low) + same + quicksort(high)
+
+
+"""
+RUNNING TIME BENCHMARK
+"""
+
+
+def shuffled_array(array):
+    """ Shuffle an array using a numpy random number generator """
+    rng = np.random.default_rng()
+    rng.shuffle(array)
+    return array
+
+
 def benchmark(input_sizes, sorting_algorithm):
     """ Find average running time (in milliseconds) 
     for a sorting algorithm for each input size array """
 
-    time_avg = []
+    running_time_average = []
 
     # For each input size generate a new array
     for i in input_sizes:
@@ -81,78 +101,87 @@ def benchmark(input_sizes, sorting_algorithm):
         for _ in range(10):
             # Shuffle the array and then sort it.
             shuffled = shuffled_array(arr)
-            _, time_diff = sorting_algorithm(shuffled)
+
+            start_time = time()
+            sorting_algorithm(shuffled)
+            time_diff = time() - start_time
 
             # Sum the time differences
             time_diff_sum += time_diff
 
         # Find the average, convert to ms,
         # round to 3 decimal places and append to the list
-        time = (time_diff_sum / 10) * 1000
-        time_avg.append(np.round(time, 3))
+        running_time_average_sec = (time_diff_sum / 10) * 1000
+        running_time_average.append(round(running_time_average_sec, 3))
 
-    return time_avg
+    return running_time_average
 
 
 def benchmark_runner(input_sizes):
-    """ Creating a list of benchmarks """
-    # Run benchmark for each sorting algorithm
-    sorting_algorithms = [insertion_sort]# , 'quicksort', 'heap_sort', 'radix_sort', 'counting_sort', 'introsort']
-
+    """ Creating a list of benchmark results """
+    
+    # A list of algorithms
+    sorting_algorithms = [insertion_sort, quicksort] # ,heap_sort, radix_sort, counting_sort, introsort]
     benchmarks = []
+
+    # Run benchmark for each sorting algorithm
     for sorting_algorithm in sorting_algorithms:
         benchmarks.append(benchmark(input_sizes, sorting_algorithm))
 
     return benchmarks
 
 
-def output_to_console(input_sizes, benchmarks):
-    """ Console output of benchmarking results """
+def results_as_dataframe(input_sizes, benchmarks):
+    """ Dateframe format of benchmarking results """
 
     # Creating a dataframe with input sizes for columns
     # and algorithm names for the index
-    algorithm_names = ['Insertion Sort']# , 'Quicksort', 'Heap Sort', 'Radix Sort', 'Counting Sort', 'IntroSort']
+    # , 'Heap Sort', 'Radix Sort', 'Counting Sort', 'IntroSort']
+    algorithm_names = ['Insertion Sort', 'Quicksort']
     df = pd.DataFrame(index=algorithm_names, columns=input_sizes)
+    df.columns.name = 'Sizes'
 
     # Add the results into the dataframe
     for i in range(len(algorithm_names)):
         df.iloc[i] = benchmarks[i]
-    plot(df)
-    save_as_table(df)
-    
     return df
 
 
+"""
+PLOT AND EXCEL FOR THE REPORT
+"""
+
+
 def plot(df):
-    # Style for plots.
-    plt.style.use('bmh')
+
+    # Save the dataframe as an excel file.
+    df.to_excel('benchmark_results.xlsx')
 
     # Rearrange the dataframe.
     df = df.T
 
+    # Style for plots.
+    plt.style.use('ggplot')
+
     # Plot and save the figure.
-    df.plot(linestyle = '--', marker = 'o')
-    plt.xlabel('Input size n')
-    plt.ylabel('Running time (milliseconds)')
-    plt.title('Sorting Benchmark')
-    plt.savefig('Benchmark.jpg', bbox_inches="tight")
-    
-
-def save_as_table(df):
-    _, ax = plt.subplots() # set size frame
-    ax.xaxis.set_visible(False)  # hide the x axis
-    ax.yaxis.set_visible(False)  # hide the y axis
-    ax.set_frame_on(False)  # no visible frame, uncomment if size is ok
-    table(ax, df, loc='upper right', colWidths=[0.15]*len(df.columns))  # where df is your data frame
-    plt.savefig('table.png', transparent=True)
+    df.plot(linestyle='--', marker='o')
+    plt.xlabel('Input size n', fontstyle='italic')
+    plt.ylabel('Running time (milliseconds)', fontstyle='italic')
+    plt.title('Sorting Benchmark', y=1.05, fontsize=13)
+    plt.legend(bbox_to_anchor=(1, 1), loc='best')
+    plt.savefig('benchmark_plot.jpg', bbox_inches="tight")
 
 
-
-
-# Driver code to test above
+# Driver code to test above.
 if __name__ == '__main__':
     input_sizes = [100, 250, 500, 750, 1000,
                    1250]  # , 2500, 3750, 5000, 6250, 7500, 8750, 10000]
 
     benchmarks = benchmark_runner(input_sizes)
-    print(output_to_console(input_sizes, benchmarks))
+    benchmark_results = results_as_dataframe(input_sizes, benchmarks)
+    plot(benchmark_results)
+
+    # Output benchmark results into the console.
+    headers = [benchmark_results.columns.name] + \
+        list(benchmark_results.columns)
+    print(benchmark_results.to_markdown(tablefmt="grid", headers=headers))
